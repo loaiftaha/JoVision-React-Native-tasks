a6dabf4d8ab4fe41b98bd4efc9a6dc5119812dd9
loaiftaha <loai.f.taha@gmail.com>
2025-10-21 05:54:26 +0300

    Task16

---
+++ App.tsx
@@ -0,0 +1,39 @@
+
+import { NewAppScreen } from '@react-native/new-app-screen';
+import {  StyleSheet, useColorScheme, View } from 'react-native';
+import Task16 from './Tasks/Task16';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaProvider>
+      <Task16></Task16>
+    </SafeAreaProvider>
+  );
+}
+
+function AppContent() {
+  const safeAreaInsets = useSafeAreaInsets();
+
+  return (
+    <View style={styles.container}>
+      <NewAppScreen
+        templateFileName="App.tsx"
+        safeAreaInsets={safeAreaInsets}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
