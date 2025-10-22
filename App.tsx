f30c1aef2cbd6fa20b9fcec98faf275f43d949d4
loaiftaha <loai.f.taha@gmail.com>
2025-10-22 07:50:48 +0300

    Task19

--- App.tsx
+++ App.tsx
@@ -3,7 +3,8 @@ import { NewAppScreen } from '@react-native/new-app-screen';
 import {  StyleSheet, useColorScheme, View } from 'react-native';
 //import Task16 from './Tasks/Task16';
 //import Task17 from './Tasks/Task17';
-import Task18 from './Tasks/Task18';
+//import Task18 from './Tasks/Task18';
+import Task19 from './Tasks/Task19';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
@@ -13,9 +14,10 @@ function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <Task18></Task18>
-    </SafeAreaProvider>
+    //<SafeAreaProvider><Task16></Task16></SafeAreaProvider>
+    //<SafeAreaProvider><Task17></Task17></SafeAreaProvider>
+    //<SafeAreaProvider><Task18></Task18></SafeAreaProvider>
+    <SafeAreaProvider><Task19></Task19></SafeAreaProvider>
   );
 }
 
